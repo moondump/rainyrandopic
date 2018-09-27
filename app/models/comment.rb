aeec0b74b86c3d76e776031a705885b11729a5d9
github username <sam.catherman@gmail.com>
2018-09-27 16:25:52 -0400

    validates img_id & content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :image
+  validates :image_id, numericality: { only_integer: true }
+  validates :content, presence: true
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :image
-
+  validates :image_id, numericality: { only_integer: true }
 end
