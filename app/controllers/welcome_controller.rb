92a1719544c8c134ba7cc47d07a0a76a480de8f0
Andrew Cohn <andrewcohn0@gmail.com>
2018-10-15 10:10:09 -0400

    generate new images for each user session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,14 @@ class WelcomeController < ActionController::Base
   def index
     respond_to do |format|
 
-      format.html { 
-        session[:image_id] ||= SecureRandom.uuid
+      format.html {
+        session[:image_id] ||= Image.create.id
         @base_url = request.protocol + request.host_with_port
         @image_id = session[:image_id]
         html = render_to_string(
           file: 'welcome/index.erb'
         )
-        render body: html 
+        render body: html
       }
     end
   end
--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,5 +1,5 @@
 class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :url, :name, :like_count, :uuid
+  attributes :id, :url, :name, :like_count
   has_many :comments
 
   def like_count
