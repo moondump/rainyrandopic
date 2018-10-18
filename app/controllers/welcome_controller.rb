c1d9cb6eb3b0f0f1ffb3392921824dba49320373
Andrew Cohn <andrewcohn0@gmail.com>
2018-10-18 13:02:56 -0400

    fixx index not displaying img id correctly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,9 @@
 class WelcomeController < ActionController::Base
-  before_action :index, only: :set_session_img_id
 
   def index
     respond_to do |format|
-
       format.html {
+        session[:image_id] = (Image.exists?(session[:image_id]) ? session[:image_id] : Image.create.id)
         @base_url = request.protocol + request.host_with_port
         @image_id = session[:image_id]
         html = render_to_string(
@@ -13,12 +12,6 @@ class WelcomeController < ActionController::Base
         render body: html
       }
     end
-  end
-
-  private
-
-  def set_session_img_id
-    session[:image_id] = (Image.exists?(session[:image_id]) ? session[:image_id] : Image.create.id)
   end
 
 end
