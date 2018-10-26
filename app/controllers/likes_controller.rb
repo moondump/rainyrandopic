0fecf409ef7bc7ec9cd3a10f09fd5a7827588998
github username <sam.catherman@gmail.com>
2018-10-26 10:18:54 -0400

    swap save for persisted

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(image_id: params[:image_id])
-    if @like.save
+    if @like.persisted?
       render json: @like
     else
       render json: { message: "Like validation failed", errors: @like.errors }, status: :unprocessable_entity
