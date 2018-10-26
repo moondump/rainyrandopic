ff750404a584405578f5d72af7befc4f24781cc3
github username <sam.catherman@gmail.com>
2018-10-26 10:15:59 -0400

    send err msgs from controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,14 @@ class CommentsController < ApplicationController
   def create
     # change parameter from `comment` to `content` in student readme
     @comment = Comment.create(content: params[:content], image_id: params[:image_id])
-
     # change response in student readme
-    
-    render json: @comment
+    if @comment.persisted?
+      render json: @comment
+    else
+      render json: { message: "Comment validation failed", errors: @comment.errors }, status: :unprocessable_entity
+    end
   end
+
 
   def destroy
     @comment = Comment.find(params[:id])
