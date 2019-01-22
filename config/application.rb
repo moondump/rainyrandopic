3da81c97b018baf5d8c924c5d0d0af83a6fb84d1
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-01-22 15:24:28 -0500

    move cors stuff to initializers and allow all verbs

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,5 @@ module ModThreeCodeChallengeBackend
     config.middleware.use ActionDispatch::Session::CookieStore, key: '_rando_pic', expire_after: 30.days
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
-      end
-    end
   end
 end
